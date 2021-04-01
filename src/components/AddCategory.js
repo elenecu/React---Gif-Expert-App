dc188f2edb121241ef69a114cc57c3c8233e9329
Nicolas Quintana <nicolasquintana94@outlook.es>
2021-03-31 22:02:54 -0300

    First Commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+
+
+
+export const AddCategory = ( {setCategories} ) => {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  };
+  
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    if ( inputValue.trim().length > 2 ) {      
+      setCategories( categ => [inputValue, ...categ] );
+      setInputValue('');
+
+    }else {
+      setInputValue('');
+      console.log( 'Debe colocar al menos 3 caracteres' )
+    };
+ 
+  };
+
+
+  return (
+
+    <form onSubmit = { handleSubmit }>
+
+      <input 
+      type = 'text'
+      value = { inputValue }   
+      onChange = { handleInputChange }   
+      />
+
+    </form>
+
+  )
+}
+
+AddCategory.propTypes = {
+ setCategories: PropTypes.func.isRequired
+}
